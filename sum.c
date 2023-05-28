d3445573622f1339bd063aa62fd8564d74814a49
HITYSHI-RAMESH <134825808+HITYSHI-RAMESH@users.noreply.github.com>
2023-05-28 17:35:46 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {    
+
+    int num1, num2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    
+    sum = num1 + num2;      
+    
+    printf("%d + %d = %d", num1, num2, sum);
+    return 0;
+}
